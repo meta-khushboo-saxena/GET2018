ba22d7e61be280684f51c694400647146ba54002
meta-khushboo-saxena <41181545+meta-khushboo-saxena@users.noreply.github.com>
2018-07-20 10:25:22 +0530

    Update PromotionEnum.java

--- PF-Assignment-2/PromotionEnum.java
+++ PF-Assignment-2/PromotionEnum.java
@@ -1,9 +1,32 @@
 package Shoppingcartdetail;
 
-public class PromotionEnum {
+public enum PromotionEnum {
 
-	 final String FixedProductPromotionCode="ABCDE";
-	 final String FixedOrderPromotionCode="EFGHI";
-	 final String StartDate="11-07-2018";
-	 final String EndDate="11-09-2018"; 
+	Enum1("ABCD", "12-01-2018", "12-10-2018"), 
+	Enum2("EFGH", "12-02-2018", "12-09-2018"), 
+	Enum3("IJKL", "12-04-2018", "12-09-2018"), 
+	Enum4("MNOP", "12-05-2018", "12-09-2018");
+
+	private String code;
+	private String startDate;
+	private String endDate;
+
+	private PromotionEnum(String code, String startDate, String endDate) {
+		this.code = code;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public String getStartDate() {
+		return startDate;
+	}
+	
+	public String getEndDate() {
+		return endDate;
+	}
+
 }
