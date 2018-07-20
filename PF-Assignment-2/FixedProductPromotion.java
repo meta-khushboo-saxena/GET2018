c1581954de861e29fd1006586a457423b38d9000
meta-khushboo-saxena <41181545+meta-khushboo-saxena@users.noreply.github.com>
2018-07-20 10:22:55 +0530

    Update FixedProductPromotion.java

--- PF-Assignment-2/FixedProductPromotion.java
+++ PF-Assignment-2/FixedProductPromotion.java
@@ -3,83 +3,62 @@ package Shoppingcartdetail;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
-public class FixedProductPromotion implements Promotion{
-	
-	    private int discount;
-	    private int minPrice;
-	    PromotionEnum promotionenum = new PromotionEnum();
-	  
-	    public void setFixedDiscount(int discount) {
-			this.discount = discount;
-		}
+public class FixedProductPromotion implements Promotion {
 
-		public void setMinimumPrice(int minPrice) {
-		    this.minPrice = minPrice;	
-		}
-		
-		public int getFixedDiscount() {
+	private int discount;
+	private int minPrice;
+
+	public void setFixedDiscount(int discount) {
+		this.discount = discount;
+	}
+
+	public void setMinimumPrice(int minPrice) {
+		this.minPrice = minPrice;
+	}
+
+	public int getFixedDiscount() {
 		return this.discount;
-		}
+	}
 
-		public int getMinimumPrice() {
-		 return this.minPrice;	
-		}
+	public int getMinimumPrice() {
+		return this.minPrice;
+	}
 
-		public boolean isPromotionApplicable(String code,Date currentDate) {
-			
-			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
-			long start = 0;
-			long end = 0;
-			long current = 0;
-			
-			  try{
-		             Date startDate = (Date) simpleDateFormat.parse(promotionenum.StartDate);
-		             Date endDate = (Date) simpleDateFormat.parse(promotionenum.EndDate);
-		
-		             start = startDate.getTime();
-	                 end = endDate.getTime();
-	                 current = currentDate.getTime();
-	                 
-		            }catch(Exception e){
-			         e.printStackTrace();
-		            }
-			
-			if(promotionenum.FixedProductPromotionCode.equals(code) && current < end && current > start)
-			{
-				//System.out.println("Promocode applied on product");
+	public boolean isPromotionApplicable(String code) {
+
+		Date date = new Date();
+
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+		long start = 0;
+		long end = 0;
+		long current = 0;
+
+		for (PromotionEnum promo : PromotionEnum.values()) {
+
+			try {
+				Date startDate = (Date) simpleDateFormat.parse(promo
+						.getStartDate());
+				Date endDate = (Date) simpleDateFormat
+						.parse(promo.getEndDate());
+
+				start = startDate.getTime();
+				end = endDate.getTime();
+				current = date.getTime();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			if (promo.getCode().equals(code) && current < end
+					&& current > start) {
+
 				return true;
 			}
-				
-				return false;	
+
 		}
 
+		return false;
+
+	}
+
 }
-
-
-
-/*public boolean isPromotionApplicable(String code, Date currentDate) {
-  	
-	promotionEnum promotionenum = new promotionEnum();
-	SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
-	long start = 0;
-	long end = 0;
-	long current = 0;
-	
-	         try{
-	             Date startDate = simpleDateFormat.parse(promotionenum.startDate);
-	             Date endDate = simpleDateFormat.parse(promotionenum.endDate);
-	
-	             start = startDate.getTime();
-                 end = endDate.getTime();
-                 current = currentDate.getTime();
-	
-	            }catch(Exception e){
-		         e.printStackTrace();
-	            }
-	
-	 if((promotionenum.code).equals(code) && current < end && current > start){
-			return true;
-     }else
-    	 return false;
-		
-}*/
