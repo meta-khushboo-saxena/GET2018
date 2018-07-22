fe14897c8517328c4ce65ead6b91bd020d9f7dbd
meta-khushboo-saxena <41181545+meta-khushboo-saxena@users.noreply.github.com>
2018-07-22 16:04:04 +0530

    Update Area.java

--- SCF-Session3/Area.java
+++ SCF-Session3/Area.java
@@ -1,147 +1,175 @@
 import java.util.Scanner;
 
-//Computes Area of Triangle,Rectangle,Circle,Square
+/**
+ * Computes Area of Triangle,Rectangle,Circle,Square
+ */
 
 public class Area {
 
-	static Scanner sc = new Scanner(System.in);
-	/**
-	 * Function to calculate area of Triangle
-	 * 
-	 * @param height,base
-	 * 
-	 * @return area
-	 */
-	double triangleArea(double height, double base) {
-		double area = (double) (0.5) * (base * height);
-		return area;
-	}
+    static Scanner sc = new Scanner(System.in);
 
-	/**
-	 * Function to calculate area of Rectangle
-	 * 
-	 * @param width,height
-	 * 
-	 * @return area
-	 */
-	double rectangleArea(double width, double height) {
-		double area = (width * height);
-		return area;
-	}
+    /**
+     * Function to calculate area of Triangle
+     * 
+     * @param height
+     *            ,base
+     * 
+     * @return area
+     */
+    double triangleArea(double height, double base) throws ArithmeticException {
+        if (Double.isInfinite(base) || Double.isInfinite(height)
+                || Double.isNaN(base) || Double.isNaN(height)) {
+            throw new ArithmeticException("Invalid double value");
+        }
+        double area = (double) (0.5) * (base * height);
+        return area;
+    }
 
-	/**
-	 * Function to calculate area of Circle
-	 * 
-	 * @param radius
-	 * 
-	 * @return area
-	 */
-	double circleArea(double radius) {
-		double area = (double) ((3.14) * radius * radius);
-		return area;
-	}
+    /**
+     * Function to calculate area of Rectangle
+     * 
+     * @param width
+     *            ,height
+     * 
+     * @return area
+     */
+    double rectangleArea(double width, double height)
+            throws ArithmeticException {
+        if (Double.isInfinite(width) || Double.isInfinite(height)
+                || Double.isNaN(width) || Double.isNaN(height)) {
+            throw new ArithmeticException("Invalid double value");
+        }
+        double area = (width * height);
+        return area;
+    }
 
-	/**
-	 * Function to calculate area of Square
-	 * 
-	 * @param sides
-	 * 
-	 * @return area
-	 */
-	double squareArea(double sides) {
-		double area = (double) (sides * sides);
-		return area;
-	}
+    /**
+     * Function to calculate area of Circle
+     * 
+     * @param radius
+     * 
+     * @return area
+     */
+    double circleArea(double radius) throws ArithmeticException {
+        if (Double.isInfinite(radius) || Double.isNaN(radius)) {
+            throw new ArithmeticException("Invalid double value");
+        }
+        double area = (double) ((3.14) * radius * radius);
+        return area;
+    }
 
-	public static void main(String args[]) {
-		int choice = 0;
-		
-		Area area = new Area();
+    /**
+     * Function to calculate area of Square
+     * 
+     * @param sides
+     * 
+     * @return area
+     */
+    double squareArea(double sides) throws ArithmeticException {
+        if (Double.isInfinite(sides) || Double.isNaN(sides)) {
+            throw new ArithmeticException("Invalid double value");
+        }
+        double area = (double) (sides * sides);
+        return area;
+    }
 
-		do {
-			try {
-				System.out.println("1. Triangle");
-				System.out.println("2. Rectangle");
-				System.out.println("3. Circle");
-				System.out.println("4. Square");
-				System.out.println("5. Exit");
+    public static void main(String args[]) {
+        int choice = 0;
 
-				System.out.println("Enter your choice : ");
-				choice = sc.nextInt();
+        Area area = new Area();
 
-				switch (choice) {
-				case 1: {
-					double base, height;
-					System.out.println("Enter Base : ");
-					base = sc.nextDouble();
-					if (base <= 0) {
-						throw new ArithmeticException();
-					}
-					System.out.println("Enter Height : ");
-					height = sc.nextDouble();
-					if (height <= 0) {
-						throw new ArithmeticException();
-					}
-					System.out.println("Area of Triangle : "
-							+ area.triangleArea(height, base));
-					break;
-				}
-				case 2: {
-					double width, height;
-					System.out.println("Enter Width : ");
-					width = sc.nextDouble();
-					if (width <= 0) {
-						throw new ArithmeticException();
-					}
-					System.out.println("Enter Height : ");
-					height = sc.nextDouble();
-					if (height <= 0) {
-						throw new ArithmeticException();
-					}
-					System.out.println("Area of Rectangle : "
-							+ area.rectangleArea(height, width));
-					break;
-				}
-				case 3: {
-					double radius;
-					System.out.println("Enter Radius : ");
-					radius = sc.nextDouble();
-					if (radius <= 0) {
-						throw new ArithmeticException();
-					}
-					System.out.println("Area of Circle : "
-							+ area.circleArea(radius));
-					break;
-				}
-				case 4: {
-					double sides;
-					System.out.println("Enter Side : ");
-					sides = sc.nextDouble();
-					if (sides <= 0) {
-						throw new ArithmeticException();
-					}
-					System.out.println("Area of Square : "
-							+ area.squareArea(sides));
-					break;
-				}
-				case 5: {
-					System.exit(0);
-				}
-				default: {
-					System.out.println("Invalid choice");
-					break;
-				}
-				}
-			} catch (ArithmeticException e) {
-				System.out.println("Input cannot be negative or Zero");
-				
+        do {
+            System.out.println("1. Triangle");
+            System.out.println("2. Rectangle");
+            System.out.println("3. Circle");
+            System.out.println("4. Square");
+            System.out.println("5. Exit");
 
-			} catch (Exception e1) {
-				System.out.println("Enter valid Input Please");
-				
-			}
-			
-		} while (true);
+            System.out.println("Enter your choice : ");
+            choice = sc.nextInt();
 
-	}
+            switch (choice) {
+            case 1: {
+                double base, height;
+
+                while (true) {
+                    System.out.println("Enter Base : ");
+                    base = sc.nextDouble();
+                    System.out.println("Enter Height : ");
+                    height = sc.nextDouble();
+
+                    if (base <= 0 || height <= 0) {
+                        System.out
+                                .println("Base and height should be greater than 0: Enter again");
+                    } else {
+                        break;
+                    }
+                }
+
+                System.out.println("Area of Triangle : "
+                        + area.triangleArea(height, base));
+                break;
+            }
+            case 2: {
+                double width, height;
+                while (true) {
+                    System.out.println("Enter Width : ");
+                    width = sc.nextDouble();
+                    System.out.println("Enter Height : ");
+                    height = sc.nextDouble();
+                    if (width <= 0 || height <= 0) {
+                        System.out
+                                .println("Width and height should be greater than 0: Enter again");
+                    } else {
+                        break;
+                    }
+                }
+                System.out.println("Area of Rectangle : "
+                        + area.rectangleArea(height, width));
+                break;
+            }
+            case 3: {
+                double radius;
+                while (true) {
+                    System.out.println("Enter Radius : ");
+                    radius = sc.nextDouble();
+                    if (radius <= 0) {
+                        System.out
+                                .println("Radius should be greater than 0: Enter again");
+                    } else {
+                        break;
+                    }
+                }
+                System.out.println("Area of Circle : "
+                        + area.circleArea(radius));
+                break;
+            }
+            case 4: {
+                double sides;
+                while (true) {
+                    System.out.println("Enter Side : ");
+                    sides = sc.nextDouble();
+                    if (sides <= 0) {
+                        System.out
+                                .println("Sides should be greater than 0: Enter again");
+                    } else {
+                        break;
+                    }
+                }
+                System.out
+                        .println("Area of Square : " + area.squareArea(sides));
+                break;
+            }
+            case 5: {
+                System.exit(0);
+            }
+            default: {
+                System.out.println("Invalid choice");
+                break;
+            }
+            }
+
+        } while (true);
+
+    }
 }
