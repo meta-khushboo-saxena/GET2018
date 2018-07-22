daa5e24179b92be7577cacd4730b53bd181e6242
meta-khushboo-saxena <41181545+meta-khushboo-saxena@users.noreply.github.com>
2018-07-22 15:20:03 +0530

    Update Marksheet.java

--- SCF-Session3/Marksheet/Marksheet.java
+++ SCF-Session3/Marksheet/Marksheet.java
@@ -2,52 +2,51 @@ package Marksheet;
 
 import java.util.Scanner;
 
+/**
+ * main class for grade calculation
+ */
 public class Marksheet {
 
-	static Scanner sc = new Scanner(System.in);
-	static Scanner sc1 = new Scanner(System.in);
+    static Scanner sc = new Scanner(System.in);
 
-	public static void main(String arg[]) {
+    public static void main(String arg[]) {
 
-		GradeCalculation marks = new GradeCalculation();
-		int n = 0;
-		float grade[] = {};
+        GradeCalculation marks = new GradeCalculation();
+        int n = 0;
+        float grade[] = {};
+        System.out.println("Enter the number of Student");
+        while (true) {
+            n = sc.nextInt();
 
-		try {
-			System.out.println("Enter the number of Student");
-			n = sc.nextInt();
-			if (n <= 0) {
-				throw new ArithmeticException();
-			}
+            if (n <= 0) {
+                System.out.println("Number should be positive: Enter again!!");
+            } else {
+                grade = new float[n];
+                break;
+            }
 
-			grade = new float[n];
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter the grades of students " + (i + 1) + ":");
+            while (true) {
+                grade[i] = sc.nextFloat();
+                if (grade[i] < 0 || grade[i] > 100) {
+                    System.out
+                            .println("Grade should be between 0 and 100: Enter grade again!!");
+                } else {
+                    break;
+                }
+            }
+        }
 
-			for (int i = 0; i < n; i++) {
+        System.out.printf("Average of all grades:" + "%.2f\n", marks.average(
+                grade, n));
+        System.out.printf("Maximum of all grades:" + "%.2f\n", marks.maximum(
+                grade, n));
+        System.out.printf("Minimum of all grades:" + "%.2f\n", marks.minimum(
+                grade, n));
+        System.out.printf("Percentage of students passed:" + "%.2f\n", marks
+                .percentage(grade, n));
 
-				System.out.println("Enter the grades of" + i + "students:");
-				grade[i] = sc1.nextFloat();
-				if (grade[i] < 0 || grade[i] > 100) { // if grade is not between
-														// 0 and 100 throw
-														// exception
-					throw new Exception("Out of range");
-				}
-			}
-
-		} catch (ArithmeticException e) {
-			System.out.println("Number of Students cannot be negative or Zero");
-			System.exit(0);
-		} catch (Exception e3) {
-			System.out.println("Range of Grades should be between (0-100)");
-			System.exit(0);
-		}
-
-		System.out.printf("Average of all grades:" + "%.2f",
-				marks.average(grade, n));
-		System.out.println();
-		System.out.println("Maximum of all grades:" + marks.maximum(grade, n));
-		System.out.println("Minimum of all grades:" + marks.minimum(grade, n));
-		System.out.printf("Percentage of students passed:" + "%.2f",
-				marks.percentage(grade, n));
-
-	}
+    }
 }
