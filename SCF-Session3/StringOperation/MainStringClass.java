ad5ee269fa5409670b4e78839f0b41a3f3ca4ba7
meta-khushboo-saxena <41181545+meta-khushboo-saxena@users.noreply.github.com>
2018-07-22 15:39:45 +0530

    Update MainStringClass.java

--- SCF-Session3/StringOperation/MainStringClass.java
+++ SCF-Session3/StringOperation/MainStringClass.java
@@ -2,83 +2,87 @@ package StringOperation;
 
 import java.util.Scanner;
 
-/*
+/**
  * contain various operation on string
  * string should not be null
  */
 public class MainStringClass {
 
-	static Scanner sc = new Scanner(System.in);
-	static Scanner sc1 = new Scanner(System.in);
+    static Scanner sc = new Scanner(System.in);
 
-	public static void main(String arg[]) {
-		StringOperations operation = new StringOperations();
+    public static void main(String arg[]) {
+        StringOperations operation = new StringOperations();
 
-		do {
+        do {
 
-			System.out.println("1. Compare two String");
-			System.out.println("2. Reverse a String");
-			System.out.println("3. Change the case of a String");
-			System.out.println("4. Find largest word in the String");
-			System.out.println("5. Exit");
-			System.out.println("Enter your choice");
+            System.out.println("1. Compare two String");
+            System.out.println("2. Reverse a String");
+            System.out.println("3. Change the case of a String");
+            System.out.println("4. Find largest word in the String");
+            System.out.println("5. Exit");
+            System.out.println("Enter your choice");
 
-			switch (sc.nextInt()) {
+            switch (sc.nextInt()) {
 
-			case 1: {
-				String string1 = "", string2 = "";
+            case 1: {
+                String string1 = new String();
+                String string2 = new String();
 
-				System.out.println("Enter two strings : ");
-				string1 = sc1.nextLine();
-				string2 = sc1.nextLine();
-				int compareString = operation.compareStrings(string1, string2);
-				if (compareString == 1) {
-					System.out.println("Both Strings are Same");
-				} else {
-					System.out.println("Both Strings are NOT Same");
-				}
-				break;
+                System.out.println("Enter two strings : ");
+                sc.nextLine();
+                string1 = sc.nextLine();
+                string2 = sc.nextLine();
+                int compareString = operation.compareStrings(string1, string2);
+                if (compareString == 1) {
+                    System.out.println("Both Strings are Same");
+                } else {
+                    System.out.println("Both Strings are NOT Same");
+                }
+                break;
 
-			}
-			case 2: {
-				String string = "";
+            }
+            case 2: {
+                String string = new String();
 
-				System.out.println("Enter the string : ");
-				string = sc1.nextLine();
+                System.out.println("Enter the string : ");
+                sc.nextLine();
+                string = sc.nextLine();
 
-				String ReversedString = operation.stringReverse(string);
-				System.out.println("Reversed String is : " + ReversedString);
-				break;
-			}
-			case 3: {
+                String ReversedString = operation.stringReverse(string);
+                System.out.println("Reversed String is : " + ReversedString);
+                break;
+            }
+            case 3: {
 
-				String string = "";
+                String string = new String();
 
-				System.out.println("Enter the string : ");
-				string = sc1.nextLine();
+                System.out.println("Enter the string : ");
+                sc.nextLine();
+                string = sc.nextLine();
 
-				String NewString = operation.CaseChange(string);
-				System.out.println("Case changed String is : " + NewString);
-				break;
+                String NewString = operation.CaseChange(string);
+                System.out.println("Case changed String is : " + NewString);
+                break;
 
-			}
-			case 4: {
-				String string = "";
+            }
+            case 4: {
+                String string = new String();
 
-				System.out.println("Enter the string : ");
-				string = sc1.nextLine();
+                System.out.println("Enter the string : ");
+                sc.nextLine();
+                string = sc.nextLine();
 
-				System.out.println(operation.LongestWordFromString(string));
-				break;
-			}
-			case 5: {
-				System.exit(0);
-			}
-			default:
-				System.out.println("Invalid choice");
+                System.out.println(operation.LongestWordFromString(string));
+                break;
+            }
+            case 5: {
+                System.exit(0);
+            }
+            default:
+                System.out.println("Invalid choice");
 
-			}
-		} while (true);
+            }
+        } while (true);
 
-	}
+    }
 }
