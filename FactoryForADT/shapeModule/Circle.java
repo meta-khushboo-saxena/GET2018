7532b4d782292dde207d0fdda09ff64e6044bae1
meta-khushboo-saxena <41181545+meta-khushboo-saxena@users.noreply.github.com>
2018-07-31 23:48:23 +0530

    Update Circle.java

--- FactoryForADT/shapeModule/Circle.java
+++ FactoryForADT/shapeModule/Circle.java
@@ -2,66 +2,71 @@ package shapeModule;
 
 import java.util.Date;
 
-public class Circle implements Shapes{
+public class Circle implements Shapes {
 
-	private int xCoordinate;
-	private int yCoordinate;
-	private int radius;
-	private Date date;
-	private String shape = "CIRCLE";
+    private int xCoordinate;
+    private int yCoordinate;
+    private int radius;
+    private Date date;
+    private String shape = "CIRCLE";
 
-	public Circle(int radius, Point point) {
-		this.radius = radius;
-		this.xCoordinate = point.getXCoordinate();
-		this.yCoordinate = point.getYCoordinate();
-		this.date = new Date();
-	}
+    public Circle(int radius, Point point) {
+        this.radius = radius;
+        this.xCoordinate = point.getXCoordinate();
+        this.yCoordinate = point.getYCoordinate();
+        this.date = new Date();
+    }
 
-	// Getter Start
-	public int getxCoordinate() {
-		return xCoordinate;
-	}
+    // Getter Start
+    public int getxCoordinate() {
+        return xCoordinate;
+    }
 
-	public int getyCoordinate() {
-		return yCoordinate;
-	}
+    public int getyCoordinate() {
+        return yCoordinate;
+    }
 
-	public int getSide() {
-		return radius;
-	}
+    public int getSide() {
+        return radius;
+    }
 
-	public Date getDate() {
-		return date;
-	}
+    public Date getDate() {
+        return date;
+    }
 
-	// Getter End
+    // Getter End
 
-	public float getArea() {
-		float area;
-		area = this.radius * this.radius;
-		return area;
-	}
+    public float getArea() {
+        float area;
+        area = this.radius * this.radius;
+        return area;
+    }
 
-	public float getPerimeter() {
-		float perimeter;
-		perimeter = 4 * this.radius;
-		return perimeter;
-	}
+    public float getPerimeter() {
+        float perimeter;
+        perimeter = 4 * this.radius;
+        return perimeter;
+    }
 
-	public String getOrigin() {
+    public String getOrigin() {
 
-		return "(" + this.xCoordinate + "," + this.yCoordinate + ")";
-	}
+        return "(" + this.xCoordinate + "," + this.yCoordinate + ")";
+    }
 
-	public String getShape() {
-		return this.shape;
-	}
+    public String getShape() {
+        return this.shape;
+    }
 
-	public boolean isEnclosed(Point point) {
-		// Point point = null;
-		// if(point.getXCoordinate() > point.xMaxCoordinate){
-		//
-		// }
-		return false;
-	}
+    public boolean isEnclosed(Point point) {
+        boolean flag = false;
+        
+        if ((point.getXCoordinate() - this.xCoordinate)
+                * (point.getXCoordinate() - this.xCoordinate)
+                + (point.getYCoordinate() - this.yCoordinate)
+                * (point.getYCoordinate() - this.yCoordinate) <= radius
+                * radius) {
+            flag = true;
+        }
+        return flag;
+    }
 }
