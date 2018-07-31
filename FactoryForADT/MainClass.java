40123930386a6ce6f6cd6c8cff42a1bb36a1c68f
meta-khushboo-saxena <41181545+meta-khushboo-saxena@users.noreply.github.com>
2018-07-31 23:50:23 +0530

    Update MainClass.java

--- FactoryForADT/MainClass.java
+++ FactoryForADT/MainClass.java
@@ -8,286 +8,238 @@ import java.util.List;
 import java.util.Scanner;
 
 public class MainClass {
-	int side, length, width, height, base, radius;
-	int xCoordinate, yCoordinate;
-	static Scanner scanner = new Scanner(System.in);
-	List<Integer> integerList = new ArrayList<Integer>();
-	Point point = new Point();
-	Screen screen = new Screen();
+    int side, length, width, side1, side2, side3, radius;
+    int xCoordinate, yCoordinate;
+    static Scanner scanner = new Scanner(System.in);
+    List<Integer> integerList = new ArrayList<Integer>();
+    Point point = new Point();
+    Screen screen = new Screen();
 
-	/**
-	 * Function to add square to screen
-	 */
-	public void addSquare() {
-		System.out.println("Enter the side of square");
-		side = scanner.nextInt();
-		integerList.add(0, side);
-		while (true) {
-			System.out
-					.println("Set point for Square on screen (X and Y) Coordinates : ");
-			xCoordinate = scanner.nextInt();
-			point.setXCoordinate(xCoordinate);
-			yCoordinate = scanner.nextInt();
-			point.setYCoordinate(yCoordinate);
+    /**
+     * Function to set the coordinates
+     */
+    public Point setCoordinates() {
+        while (true) {
+            System.out
+                    .println("Set point for Square on screen (X and Y) Coordinates : ");
+            xCoordinate = scanner.nextInt();
+            point.setXCoordinate(xCoordinate);
+            yCoordinate = scanner.nextInt();
+            point.setYCoordinate(yCoordinate);
 
-			if (xCoordinate > point.xMaxCoordinate
-					|| xCoordinate < point.xMinCoordinate
-					|| yCoordinate > point.yMaxCoordinate
-					|| yCoordinate < point.yMinCoordinate) {
-				System.out
-						.println("Coordinates should be between 0 and 500!!\n");
-			} else {
-				break;
-			}
-		}
-		// Setting the size , x-coordinate and y-coordinate of square
-		Shapes shapeSquare = ShapeFactory.shapeType(
-				ShapeTypeEnum.SQUARE.name(), point, integerList);
+            if (xCoordinate > point.xMaxCoordinate
+                    || xCoordinate < point.xMinCoordinate
+                    || yCoordinate > point.yMaxCoordinate
+                    || yCoordinate < point.yMinCoordinate) {
+                System.out
+                        .println("Coordinates should be between 0 and 500!!\n");
+            } else {
+                break;
+            }
+        }
+        return point;
+    }
 
-		screen.addShape(shapeSquare);
-	}
+    /**
+     * Function to add square to screen
+     */
+    public void addSquare() {
+        System.out.println("Enter the side of square");
+        side = scanner.nextInt();
+        integerList.add(0, side);
+        point = setCoordinates();
+        // Setting the size , x-coordinate and y-coordinate of square
+        Shapes shapeSquare = ShapeFactory.shapeType(
+                ShapeTypeEnum.SQUARE.name(), point, integerList);
 
-	/**
-	 * Function to add rectangle to screen
-	 */
-	public void addRectangle() {
+        screen.addShape(shapeSquare);
+    }
 
-		System.out.println("Enter the length of rectangle");
-		length = scanner.nextInt();
-		integerList.add(0, length);
-		System.out.println("Enter the width of rectangle");
-		width = scanner.nextInt();
-		integerList.add(1, width);
-		while (true) {
-			System.out
-					.println("Set point for Square on screen (X and Y) Coordinates : ");
-			xCoordinate = scanner.nextInt();
-			point.setXCoordinate(xCoordinate);
-			yCoordinate = scanner.nextInt();
-			point.setYCoordinate(yCoordinate);
+    /**
+     * Function to add rectangle to screen
+     */
+    public void addRectangle() {
 
-			if (xCoordinate > point.xMaxCoordinate
-					|| xCoordinate < point.xMinCoordinate
-					|| yCoordinate > point.yMaxCoordinate
-					|| yCoordinate < point.yMinCoordinate) {
-				System.out
-						.println("Coordinates should be between 0 and 500!!\n");
-			} else {
-				break;
-			}
-		}
+        System.out.println("Enter the length of rectangle");
+        length = scanner.nextInt();
+        integerList.add(0, length);
+        System.out.println("Enter the width of rectangle");
+        width = scanner.nextInt();
+        integerList.add(1, width);
+        point = setCoordinates();
 
-		// Setting the size , x-coordinate and y-coordinate of square
-		Shapes shapeRectangle = ShapeFactory.shapeType(
-				ShapeTypeEnum.RECTANGLE.name(), point, integerList);
+        // Setting the size , x-coordinate and y-coordinate of square
+        Shapes shapeRectangle = ShapeFactory.shapeType(ShapeTypeEnum.RECTANGLE
+                .name(), point, integerList);
 
-		screen.addShape(shapeRectangle);
-	}
+        screen.addShape(shapeRectangle);
+    }
 
-	/**
-	 * Function to add Triangle to screen
-	 */
-	public void addTriangle() {
-		System.out.println("Enter the height of triangle");
-		height = scanner.nextInt();
-		integerList.add(0, height);
+    /**
+     * Function to add Triangle to screen
+     */
+    public void addTriangle() {
+        System.out.println("Enter the side1 of triangle");
+        side1 = scanner.nextInt();
+        integerList.add(0, side1);
 
-		System.out.println("Enter the base of triangle");
-		base = scanner.nextInt();
-		integerList.add(1, base);
+        System.out.println("Enter the side2 of triangle");
+        side2 = scanner.nextInt();
+        integerList.add(1, side2);
 
-		while (true) {
-			System.out
-					.println("Set point for Square on screen (X and Y) Coordinates : ");
-			xCoordinate = scanner.nextInt();
-			point.setXCoordinate(xCoordinate);
-			yCoordinate = scanner.nextInt();
-			point.setYCoordinate(yCoordinate);
+        System.out.println("Enter the side3 of triangle");
+        side3 = scanner.nextInt();
+        integerList.add(2, side3);
 
-			if (xCoordinate > point.xMaxCoordinate
-					|| xCoordinate < point.xMinCoordinate
-					|| yCoordinate > point.yMaxCoordinate
-					|| yCoordinate < point.yMinCoordinate) {
-				System.out
-						.println("Coordinates should be between 0 and 500!!\n");
-			} else {
-				break;
-			}
-		}
+        point = setCoordinates();
 
-		// Setting the height, base , x-coordinate and y-coordinate of square
-		Shapes shapeTriangle = ShapeFactory.shapeType(
-				ShapeTypeEnum.TRIANGLE.name(), point, integerList);
+        // Setting the height, base , x-coordinate and y-coordinate of square
+        Shapes shapeTriangle = ShapeFactory.shapeType(ShapeTypeEnum.TRIANGLE
+                .name(), point, integerList);
 
-		screen.addShape(shapeTriangle);
-	}
+        screen.addShape(shapeTriangle);
+    }
 
-	/**
-	 * Function to add Circle to screen
-	 */
-	public void addCircle() {
+    /**
+     * Function to add Circle to screen
+     */
+    public void addCircle() {
 
-		System.out.println("Enter the radius of Circle");
-		radius = scanner.nextInt();
-		integerList.add(0, radius);
+        System.out.println("Enter the radius of Circle");
+        radius = scanner.nextInt();
+        integerList.add(0, radius);
 
-		while (true) {
-			System.out
-					.println("Set point for Square on screen (X and Y) Coordinates : ");
-			xCoordinate = scanner.nextInt();
-			point.setXCoordinate(xCoordinate);
-			yCoordinate = scanner.nextInt();
-			point.setYCoordinate(yCoordinate);
+        point = setCoordinates();
 
-			if (xCoordinate > point.xMaxCoordinate
-					|| xCoordinate < point.xMinCoordinate
-					|| yCoordinate > point.yMaxCoordinate
-					|| yCoordinate < point.yMinCoordinate) {
-				System.out
-						.println("Coordinates should be between 0 and 500!!\n");
-			} else {
-				break;
-			}
-		}
+        // Setting the radius , x-coordinate and y-coordinate of square
+        Shapes shapeCircle = ShapeFactory.shapeType(
+                ShapeTypeEnum.CIRCLE.name(), point, integerList);
 
-		// Setting the radius , x-coordinate and y-coordinate of square
-		Shapes shapeCircle = ShapeFactory.shapeType(
-				ShapeTypeEnum.CIRCLE.name(), point, integerList);
+        screen.addShape(shapeCircle);
+    }
 
-		screen.addShape(shapeCircle);
-	}
+    /**
+     * Function to display list of Shapes in screen
+     * 
+     * @throws Exception
+     */
+    private void displayShapes() throws Exception {
+        screen.displayShapes();
+    }
 
-	/**
-	 * Function to display list of Shapes in screen
-	 * 
-	 * @throws Exception
-	 */
-	private void displayShapes() throws Exception {
-		screen.displayShapes();
-	}
+    /**
+     * Function to remove Shape from screen
+     * 
+     * @throws Exception
+     */
+    private void removeShapes(int index) {
+        screen.removeShapes(index);
+    }
 
-	/**
-	 * Function to remove Shape from screen
-	 * 
-	 * @throws Exception
-	 */
-	private void removeShapes(int index) {
-		screen.removeShapes(index);
-	}
-	
-	private void removeAllShapes() {
+    /**
+     * Function to remove all shapes from screen
+     */
+    private void removeAllShapes() {
         screen.removeAllShapes();
     }
-	
-	private void displayinAscending() throws Exception {
-        String display[]=screen.displayinAscending();
+
+    /**
+     * Function to display name of shapes in ascending order
+     * 
+     * @throws Exception
+     */
+    private void displayinAscending() throws Exception {
+        String display[] = screen.displayinAscending();
         for (int i = 0; i < display.length; i++) {
             System.out.println(display[i]);
         }
     }
 
-	/**
-	 * Function to display enclosed points in the shape
-	 */
-	public void displayEnclosedShape() {
-		while (true) {
-			System.out
-					.println("Set point for Square on screen (X and Y) Coordinates : ");
-			xCoordinate = scanner.nextInt();
-			point.setXCoordinate(xCoordinate);
-			yCoordinate = scanner.nextInt();
-			point.setYCoordinate(yCoordinate);
+    /**
+     * Function to display enclosed points in the shape
+     */
+    public void displayEnclosedShape() {
+        point = setCoordinates();
 
-			if (xCoordinate > point.xMaxCoordinate
-					|| xCoordinate < point.xMinCoordinate
-					|| yCoordinate > point.yMaxCoordinate
-					|| yCoordinate < point.yMinCoordinate) {
-				System.out
-						.println("Coordinates should be between 0 and 500!!\n");
-			} else {
-				break;
-			}
-		}
+    }
 
-	}
+    public static void main(String arg[]) {
 
-	public static void main(String arg[]) {
+        int choice;
+        int index;
+        MainClass main = new MainClass();
 
-		int choice;
-		int index;
-		MainClass main = new MainClass();
+        do {
+            try {
+                System.out.println("1. Add Square");
+                System.out.println("2. Add Rectangle");
+                System.out.println("3. Add Triangle");
+                System.out.println("4. Add Circle");
+                System.out.println("5. Display Shapes");
+                System.out.println("6. Display Shapes in Ascending Order");
+                System.out.println("7. Display Shapes enclosing a point");
+                System.out.println("8. Remove");
+                System.out.println("9. Remove all");
+                System.out.println("10. Exit");
 
-		do {
-			try {
-				System.out.println("1. Add Square");
-				System.out.println("2. Add Rectangle");
-				System.out.println("3. Add Triangle");
-				System.out.println("4. Add Circle");
-				System.out.println("5. Display Shapes");
-				System.out.println("6. Display Shapes in Ascending Order");
-				System.out.println("7. Display Shapes enclosing a point");
-				System.out.println("8. Remove");
-				System.out.println("9. Remove all");
-				System.out.println("10. Exit");
+                System.out.println("Enter your choice");
+                choice = scanner.nextInt();
 
-				System.out.println("Enter your choice");
-				choice = scanner.nextInt();
-
-				switch (choice) {
-				case 1: {
-					main.addSquare();
-					break;
-				}
-				case 2: {
-					main.addRectangle();
-					break;
-				}
-				case 3: {
-					main.addTriangle();
-					break;
-				}
-				case 4: {
-					main.addCircle();
-					break;
-				}
-				case 5: {
-					main.displayShapes();
-					break;
-				}
-				case 6: {
-				    main.displayinAscending();
-					break;
-				}
-				case 7: {
+                switch (choice) {
+                case 1: {
+                    main.addSquare();
+                    break;
+                }
+                case 2: {
+                    main.addRectangle();
+                    break;
+                }
+                case 3: {
+                    main.addTriangle();
+                    break;
+                }
+                case 4: {
+                    main.addCircle();
+                    break;
+                }
+                case 5: {
+                    main.displayShapes();
+                    break;
+                }
+                case 6: {
+                    main.displayinAscending();
+                    break;
+                }
+                case 7: {
 
                     break;
                 }
-				case 8: {
-					main.displayShapes();
-					scanner.nextLine();
-					System.out
-							.println("Enter the index which you wanna delete:");
-					index = scanner.nextInt();
-					main.removeShapes(index);
-					break;
-				}
-				case 9: {
-				    main.removeAllShapes();
-					break;
-				}
-				case 10: {
-					System.exit(0);
-					break;
-				}
-				}
+                case 8: {
+                    main.displayShapes();
+                    scanner.nextLine();
+                    System.out
+                            .println("Enter the index which you wanna delete:");
+                    index = scanner.nextInt();
+                    main.removeShapes(index);
+                    break;
+                }
+                case 9: {
+                    main.removeAllShapes();
+                    break;
+                }
+                case 10: {
+                    System.exit(0);
+                    break;
+                }
+                }
 
-			} catch (Exception e) {
-				//e.printStackTrace();
-				System.out.println("Please enter correct Input !! ");
-				scanner.nextLine();
-			}
-		} while (true);
+            } catch (Exception e) {
+                // e.printStackTrace();
+                System.out.println("Please enter correct Input !! ");
+                scanner.nextLine();
+            }
+        } while (true);
 
-	}
+    }
 
 }
