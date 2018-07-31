55ec808ff29cd4c3b5dea820ab543bb7467d3e0d
meta-khushboo-saxena <41181545+meta-khushboo-saxena@users.noreply.github.com>
2018-07-31 23:51:08 +0530

    Update ShapeFactory.java

--- FactoryForADT/shapeFactory/ShapeFactory.java
+++ FactoryForADT/shapeFactory/ShapeFactory.java
@@ -12,43 +12,44 @@ import shapeModule.Shapes;
 
 public class ShapeFactory {
 
-	public static Shapes shapeType(String shapeType, Point point,
-			List<Integer> integerList) {
-		Shapes shape = null;
-		try {
+    public static Shapes shapeType(String shapeType, Point point,
+            List<Integer> integerList) {
+        Shapes shape = null;
+        try {
 
-			if (shapeType.equalsIgnoreCase(ShapeTypeEnum.SQUARE.name())) {
-		
-				int side = integerList.get(0);
-				return new Square(side, point);
+            if (shapeType.equalsIgnoreCase(ShapeTypeEnum.SQUARE.name())) {
 
-			} else if (shapeType.equalsIgnoreCase(ShapeTypeEnum.RECTANGLE
-					.name())) {
-			 
-				int length = integerList.get(0);
-				int width = integerList.get(1);
-				return new Rectangle(length, width, point);
+                int side = integerList.get(0);
+                return new Square(side, point);
 
-			} else if (shapeType
-					.equalsIgnoreCase(ShapeTypeEnum.TRIANGLE.name())) {
-			 
-				int height = integerList.get(0);
-				int base = integerList.get(1);
-				return new Triangle(height, base, point);
+            } else if (shapeType.equalsIgnoreCase(ShapeTypeEnum.RECTANGLE
+                    .name())) {
 
-			} else if (shapeType.equalsIgnoreCase(ShapeTypeEnum.CIRCLE.name())) {
-			  
-				int radius = integerList.get(0);
-				return new Circle(radius, point);
-			} else {
-				System.out.println("No such shape found");
-			}
+                int length = integerList.get(0);
+                int width = integerList.get(1);
+                return new Rectangle(length, width, point);
 
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("!!Invalid !!");
-		}
-		return shape;
+            } else if (shapeType
+                    .equalsIgnoreCase(ShapeTypeEnum.TRIANGLE.name())) {
 
-	}
+                int side1 = integerList.get(0);
+                int side2 = integerList.get(1);
+                int side3 = integerList.get(2);
+                return new Triangle(side1, side2, side3, point);
+
+            } else if (shapeType.equalsIgnoreCase(ShapeTypeEnum.CIRCLE.name())) {
+
+                int radius = integerList.get(0);
+                return new Circle(radius, point);
+            } else {
+                System.out.println("No such shape found");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("!!Invalid !!");
+        }
+        return shape;
+
+    }
 }
