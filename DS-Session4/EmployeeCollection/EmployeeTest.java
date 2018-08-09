5e70a55f7a98c17bc8664b00a60689ffd40826c7
meta-khushboo-saxena <41181545+meta-khushboo-saxena@users.noreply.github.com>
2018-08-09 09:01:31 +0530

    Update EmployeeTest.java

--- DS-Session4/EmployeeCollection/EmployeeTest.java
+++ DS-Session4/EmployeeCollection/EmployeeTest.java
@@ -12,22 +12,22 @@ public class EmployeeTest {
 
 	@Before
 	public void testAddEmployee() {
-		employee.addEmployee("khushboo", 23, "sdsfd");
-		employee.addEmployee("Ananya", 44, "sdsfd");
-		employee.addEmployee("Richa", 66, "sdsfd");
-		employee.addEmployee("Himanshi", 77, "sdsfd");
-		employee.addEmployee("Prerna", 12, "sdsfd");
+		employee.addEmployee("Khushboo", 23, "Aligarh");
+		employee.addEmployee("Ananya", 44, "Patna");
+		employee.addEmployee("Richa", 66, "Jaipur");
+		employee.addEmployee("Himanshi", 77, "Delhi");
+		employee.addEmployee("Prerna", 12, "Noida");
 	}
 
 	@Test
 	public void testAddEmployee1() {
-		boolean result = employee.addEmployee("Nikhil", 12, "sdsfd");
+		boolean result = employee.addEmployee("Nikhil", 12, "Aligarh");
 		assertEquals(false, result);
 	}
 
 	@Test
 	public void testAddEmployee2() {
-		boolean result = employee.addEmployee("Somi", 45, "sdsfd");
+		boolean result = employee.addEmployee("Somi", 45, "Jaipur");
 		assertEquals(true, result);
 	}
 
@@ -35,19 +35,22 @@ public class EmployeeTest {
 	public void testNaturalSorting() {
 
 		List<Employee> employeeList = employee.naturalSorting();
-		employeeList.get(0).getId();
-
 		assertEquals(employeeList.get(0).getId(), 12);
 		assertEquals(employeeList.get(1).getId(), 23);
+		assertEquals(employeeList.get(2).getId(), 44);
+		assertEquals(employeeList.get(3).getId(), 66);
+		assertEquals(employeeList.get(4).getId(), 77);
 	}
 
 	@Test
 	public void testSotingByName() {
 
 		List<Employee> employeeList = employee.sotingByName();
-		employeeList.get(0).getId();
-
 		assertEquals(employeeList.get(0).getName(), "Ananya");
 		assertEquals(employeeList.get(1).getName(), "Himanshi");
+		assertEquals(employeeList.get(2).getName(), "Khushboo");
+		assertEquals(employeeList.get(3).getName(), "Prerna");
+		assertEquals(employeeList.get(4).getName(), "Richa");
 	}
+
 }
