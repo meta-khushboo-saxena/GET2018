85c8124e5cdaba55c7d2b259edc95b57c1f011c1
meta-khushboo-saxena <41181545+meta-khushboo-saxena@users.noreply.github.com>
2018-08-19 19:43:42 +0530

    Update Assignment3.sql

--- DBMS-Session2/Assignment3.sql
+++ DBMS-Session2/Assignment3.sql
@@ -1,83 +1,86 @@
 USE storefrontdb;
 
 # Q. Display Recent 50 Orders placed (Id, Order Date, Order Total).
+
+    SELECT o.user_id, o.id AS order_id,o.order_date, SUM(p.product_price) AS total_amount
+    FROM order_items i
+    INNER JOIN orders o
+    ON i.order_id = o.id
+    INNER JOIN product p
+    ON p.id = i.product_id
+    GROUP BY i.order_id
+    ORDER BY order_date DESC
+    LIMIT 50;
     
-   SELECT O.OrderId,O.Order_Date ,SUM(I.Quantity * P.Product_Price) as Order_Total 
-   FROM OrderItems I
-   INNER JOIN Orders O 
-   ON I.OrderId = O.OrderId
-   INNER JOIN Product P
-   ON I.ProductId = P.ProductId
-   GROUP BY OrderId
-   ORDER BY Order_Date DESC;
-
 # Q. Display 10 most expensive Orders.
-
-   SELECT O.OrderId,O.Order_Date ,SUM(I.Quantity * P.Product_Price) as Order_Total 
-   FROM OrderItems I
-   INNER JOIN Orders O 
-   ON I.OrderId = O.OrderId
-   INNER JOIN Product P
-   ON I.ProductId = P.ProductId
-   GROUP BY OrderId
-   ORDER BY Order_Total DESC
-   LIMIT 10;
+   
+    SELECT o.user_id, o.id AS order_id,o.order_date, SUM(p.product_price) AS total_amount
+    FROM order_items i
+    INNER JOIN orders o
+    ON i.order_id = o.id
+    INNER JOIN product p
+    ON p.id = i.product_id
+    GROUP BY i.order_id
+    ORDER BY total_amount DESC
+    LIMIT 10;
 
 # Q. Display all the Orders which are placed more than 10 days old and one or more items from those orders are still not shipped.
     
-    SELECT OrderId,Order_ShippingDate,Order_Date
-    FROM Orders
-    WHERE Order_Date < CURDATE() - INTERVAL 10 DAY
-    AND OrderId IN (SELECT OrderId
-					 FROM OrderItems
-                     WHERE OrderStatus = 'Not Shipped');
-                     
+    SELECT id,order_date
+    FROM orders
+    WHERE order_date < CURDATE() - INTERVAL 10 DAY
+    AND id IN (SELECT order_id
+					 FROM order_items
+                     WHERE order_status = 'Not Shipped');
+               
 # Q. Display list of shoppers which havent ordered anything since last month.
 
-    SELECT u.UserId, u.User_firstname, u.User_lastname
-    FROM User u
-    WHERE u.UserId NOT IN (
-                       SELECT ShopperId 
-                       FROM Orders
-                       WHERE Order_Date >= CURDATE() - INTERVAL 30 DAY
+    SELECT id, firstname, lastname
+    FROM user
+    WHERE id NOT IN (
+                       SELECT user_id 
+                       FROM orders
+                       WHERE order_date >= CURDATE() - INTERVAL 30 DAY
                        );
-                       
+    
 # Q. Display list of shopper along with orders placed by them in last 15 days. 
 
-    SELECT U.User_firstname,U.User_lastname ,O.OrderId,O.Order_Date
-    FROM Orders O
-    INNER JOIN User U
-    ON U.UserId = O.ShopperId
-    WHERE O.Order_Date <= CURDATE()
-    AND O.Order_Date > CURDATE() - INTERVAL 15 DAY;
+    SELECT u.firstname,u.lastname ,o.id AS order_id,o.order_date
+    FROM orders o
+    INNER JOIN user u
+    ON u.id = o.user_id
+    WHERE o.order_date > CURDATE() - INTERVAL 15 DAY;
 
 # Q. Display list of order items which are in “shipped” state for particular Order Id (i.e.: 1020))
     
-    SELECT P.Product_Name
-    FROM OrderItems O,Product P 
-    WHERE O.ProductId = P.ProductId AND 
-    OrderId = 15 AND
-    OrderStatus = "Shipped";
+    SELECT p.product_name
+    FROM order_items o,product p 
+    WHERE o.product_id = p.id AND 
+    o.id = 6 AND
+    o.order_status = 'Shipped';
 
 # Q. Display list of order items along with order placed date which fall between Rs 20 to Rs 5000 price.
 
-    SELECT I.OrderId, I.ProductId, O.Order_Date, P.Product_Price
-    FROM OrderItems I
-    INNER JOIN Orders O
-    ON I.OrderId = O.OrderId
-    INNER JOIN Product P
-    ON I.ProductId = P.ProductId
-    WHERE P.Product_Price BETWEEN 20 AND 5000;
+    SELECT i.order_id, i.product_id, o.order_date, p.product_price
+    FROM order_items i
+    INNER JOIN orders o
+    ON i.order_id = o.id
+    INNER JOIN product p
+    ON i.product_id = p.id
+    WHERE p.product_price BETWEEN 20 AND 5000;
     
 # Q. Update first 20 Order items status to “Shipped” which are placed today.
 
-    UPDATE OrderItems
-    SET OrderStatus = "Shipped"
-    WHERE OrderId IN(
-                        SELECT OrderId
-                        FROM Orders
-                        WHERE Order_Date = CURDATE() 
+    SET SQL_SAFE_UPDATES = 0 ;
+
+    UPDATE order_items
+    SET order_status = "Shipped"
+    WHERE order_id IN(
+                        SELECT id
+                        FROM orders
+                        WHERE order_date = CURDATE()
+                        ORDER BY order_date
                              )
     LIMIT 20;
     
-    SELECT * FROM OrderItems;
+    SELECT * FROM order_items;
