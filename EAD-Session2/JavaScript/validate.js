9fc1f437f33846c36593cc0138f6ba2619f3ce0e
meta-khushboo-saxena <41181545+meta-khushboo-saxena@users.noreply.github.com>
2018-08-29 15:52:14 +0530

    Update validate.js

--- EAD-Session2/JavaScript/validate.js
+++ EAD-Session2/JavaScript/validate.js
@@ -1,24 +1,41 @@
 function validateAll(){
-    if(validateName(document.getElementById('first_name')) && validateName(document.getElementById('last_name')) && validateContact() && validateEmail() && validatePassword() && checkPasswordMatch()){
-		return true;
-	}else{
-		alert("Please enter all the feild correctly!!");
-		return false;	
-	}	
+	var flag = true;
+	if(validateName(document.getElementById('last_name'))){
+		flag = false;
+	}
+	if(validateName(document.getElementById('last_name'))){
+		flag = false;
+	}
+	if(validateContact()){
+		flag = false;
+	}
+	if(validateEmail()){
+		flag = false;
+	}
+	if(validatePassword()){
+		flag = false;
+	}
+	if(checkPasswordMatch()){
+		flag = false;
+	}
+	return flag;
 }
 
 function validateName(name){
+	var flag = false;
 	if(!(/^[a-zA-Z]{2,}$/.test(name.value))){
 		name.style.borderColor="red";
-		 if(name.value == document.getElementById('first_name').value){
-			 document.getElementById('checkfirstname').innerHTML = "*invalid firstname";
+		 if(name.value == document.getElementById('first_name').value && name.value != ""){
+			 document.getElementById('checkfirstname').innerHTML = "invalid firstname";
+			 document.getElementById('checkfirstname').style.color = "red";
+		 }else if (name.value == document.getElementById('first_name').value && name.value == ""){
+			 document.getElementById('checkfirstname').innerHTML = "enter firstname";
 			 document.getElementById('checkfirstname').style.color = "red";
 		 }else{
-			 document.getElementById('checklastname').innerHTML = "*invalid lastname";
+			 document.getElementById('checklastname').innerHTML = "invalid lastname";
 			 document.getElementById('checklastname').style.color = "red";
 		 }
-		return false;
-	}else{
+	}else {
 		 if(name.value == document.getElementById('first_name').value){
 			 document.getElementById('checkfirstname').innerHTML = "&#10004;"; 
 			 document.getElementById('checkfirstname').style.color = "green";
@@ -27,68 +44,86 @@ function validateName(name){
 			 document.getElementById('checklastname').style.color = "green";
 		 }
 		name.style.borderColor="green";		 
-		return true;
-	}	
+		flag = true;
+	}
+		return flag;
 }
 
 function validateContact(){
-	if(!(/^[0-9]{8,12}$/.test(document.getElementById('contact').value))){
-		document.getElementById('checkcontact').innerHTML = "*invalid number";
+	var flag = false;
+	var contact = document.getElementById('contact').value;
+	if(!(/^[0-9]{8,12}$/.test(contact)) && contact != ""){
+		document.getElementById('checkcontact').innerHTML = "invalid number";
 		document.getElementById('checkcontact').style.color = "red";
 		document.getElementById('contact').style.borderColor="red";
-		return false;
+	}else if(!(/^[0-9]{8,12}$/.test(contact)) && contact == ""){
+		document.getElementById('checkcontact').innerHTML = "enter number";
+		document.getElementById('checkcontact').style.color = "red";
+		document.getElementById('contact').style.borderColor="red";
 	}else{
 		document.getElementById('checkcontact').innerHTML = "&#10004;";
 		document.getElementById('checkcontact').style.color = "green";
 		document.getElementById('contact').style.borderColor="green";
-		return true;
+		flag = true;
 	}
+	return flag;
 }
 				
 function validateEmail() {
- if (!(/^\w+([\.-]?\w+)*@[a-zA-Z]+(\.[a-zA-Z]{2,3})+$/.test(document.getElementById('email').value)))
-  {
-	document.getElementById('email').style.borderColor = "red";
-	document.getElementById('checkemail').innerHTML = "*invalid email";
-	document.getElementById('checkemail').style.color = "red";
-    return false;
-	
-  }else{
-	document.getElementById('checkemail').innerHTML = "&#10004;";
-	document.getElementById('checkemail').style.color = "green";
-    document.getElementById('email').style.borderColor = "green";
-    return true;
-}
+	var flag = false;
+	var email = document.getElementById('email').value;
+	if (!(/^\w+([\.-]?\w+)*@[a-zA-Z]+(\.[a-zA-Z]{2,3})+$/.test(email)) && email != ""){
+			document.getElementById('email').style.borderColor = "red";
+			document.getElementById('checkemail').innerHTML = "invalid email";
+			document.getElementById('checkemail').style.color = "red";	
+		}else if (!(/^\w+([\.-]?\w+)*@[a-zA-Z]+(\.[a-zA-Z]{2,3})+$/.test(email)) && email == ""){
+			document.getElementById('email').style.borderColor = "red";
+			document.getElementById('checkemail').innerHTML = "enter email";
+			document.getElementById('checkemail').style.color = "red";	
+		}else{
+			document.getElementById('checkemail').innerHTML = "&#10004;";
+			document.getElementById('checkemail').style.color = "green";
+			document.getElementById('email').style.borderColor = "green";
+			flag = true;
+		}
+		return flag;
 }
 
  function validatePassword() {
-	 if (/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{8,}$/.test(document.getElementById('password').value))
-  {
+	var flag = false;
+	var password = document.getElementById('password').value;
+	if (!(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{8,}$/.test(password)) && password != "") {
+	  document.getElementById('checkpassword').innerHTML = "weak";
+	  document.getElementById('checkpassword').style.color = "red";
+	  document.getElementById('password').style.borderColor="red";
+	} else if (!(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{8,}$/.test(password)) && password == ""){
+	  document.getElementById('checkpassword').innerHTML = "enter password";
+	  document.getElementById('checkpassword').style.color = "red";
+	  document.getElementById('password').style.borderColor="red";
+	} else {
 	  document.getElementById('password').style.borderColor="green";
 	  document.getElementById('checkpassword').innerHTML = "strong";
 	  document.getElementById('checkpassword').style.color = "green";
-		return true;
-  } else{
-	  document.getElementById('checkpassword').innerHTML = "*weak";
-	  document.getElementById('checkpassword').style.color = "red";
-	  document.getElementById('password').style.borderColor="red";
-    return false;
-  }
+	  flag = true;
+	}
+	return flag;
 }
 
 function checkPasswordMatch() {
+	var flag = false;
     var password = document.getElementById('password').value;
     var confirmPassword = document.getElementById('confirm_password').value;
-    if (password != confirmPassword){
-        document.getElementById('checkconfirmpassword').innerHTML = "&#10006";
-		document.getElementById('checkconfirmpassword').style.color = "red";
-		document.getElementById('confirm_password').style.borderColor="red";	
-		return false;
+	if(password != ""){
+		if (password != confirmPassword){
+			document.getElementById('checkconfirmpassword').innerHTML = "&#10006";
+			document.getElementById('checkconfirmpassword').style.color = "red";
+			document.getElementById('confirm_password').style.borderColor="red";	
+		} else {
+			document.getElementById('checkconfirmpassword').innerHTML = "&#10004;";
+			document.getElementById('checkconfirmpassword').style.color = "green";
+			document.getElementById('confirm_password').style.borderColor="green";
+			flag = true;
+		}
 	}
-    else{
-        document.getElementById('checkconfirmpassword').innerHTML = "&#10004;";
-		document.getElementById('checkconfirmpassword').style.color = "green";
-		document.getElementById('confirm_password').style.borderColor="green";
-		return true;
-	}
+	return flag;
 }  
