c5d50c3e7d34a77fec3a6946ad60b2f7cbfc9fa0
meta-khushboo-saxena <41181545+meta-khushboo-saxena@users.noreply.github.com>
2018-09-11 00:11:56 +0530

    Update EmployeeController.java

--- EmployeePortal/src/main/java/com/metacube/training/controller/EmployeeController.java
+++ EmployeePortal/src/main/java/com/metacube/training/controller/EmployeeController.java
@@ -39,6 +39,12 @@ public class EmployeeController {
 		return "employee/login";
 	}
 
+	/**
+	 * Function to validate login page
+	 * @param:email
+	 * @param:password
+	 * @return string
+	 */
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public String login(@RequestParam(name = "email") String email,
 			@RequestParam(name = "password") String password, Model model) {
@@ -100,104 +106,5 @@ public class EmployeeController {
 		}
 		return view;
 	}
-	// @RequestMapping(value = "/login", method = RequestMethod.GET)
-	// public String login() {
-	//
-	// return "employee/login";
-	// }
-	//
-	// @RequestMapping(value = "/login", method = RequestMethod.POST)
-	// public ModelAndView login(@RequestParam("username") String username,
-	// @RequestParam("password") String password){
-	//
-	// String view;
-	// EmployeeService employeeService = EmployeeServiceImpl.getInstance();
-	//
-	// if(employeeService.isValidLogin(username, password))
-	// view = "employee/dashboard";
-	// else
-	// view = "employee/login";
-	//
-	// return new ModelAndView(view, "email", username);
-	// }
-	//
-	// @RequestMapping(value = "/editProfile", method = RequestMethod.GET)
-	// public ModelAndView addEmployee(@RequestParam String email){
-	//
-	// EmployeeService employeeService = EmployeeServiceImpl.getInstance();
-	//
-	// return new ModelAndView("employee/editProfile", "employee",
-	// employeeService.getEmployeeByEmail(email));
-	// }
-	//
-	// @RequestMapping(value = "/editProfile", method = RequestMethod.POST)
-	// public ModelAndView addEmployee(@RequestParam("employeeCode") String
-	// employeeCode, @RequestParam("firstName") String firstName,
-	// @RequestParam("middleName") String middleName,
-	// @RequestParam("lastName") String lastName, @RequestParam("email") String
-	// email, @RequestParam("dob") String dob,
-	// @RequestParam("gender") char gender, @RequestParam("primaryContact")
-	// String primaryContact, @RequestParam("secondaryContact") String
-	// secondaryContact,
-	// @RequestParam("skypeId") String skypeId, @RequestParam("skills") String[]
-	// skills,@RequestParam("oldPassword") String oldPassword,
-	// @RequestParam("password") String password,
-	// @RequestParam("confirmPassword") String confirmPassword,
-	// @RequestParam("enabled") boolean enabled) throws ParseException{
-	//
-	// SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-	// Date dateOfBirth = format.parse(dob);
-	//
-	// Employee employee = new Employee();
-	// employee.setEmployeeCode(employeeCode);
-	// employee.setFirstName(firstName);
-	// employee.setMiddleName(middleName);
-	// employee.setLastName(lastName);
-	// employee.setEmail(email);
-	// employee.setDob(dateOfBirth);
-	// employee.setGender(gender);
-	// employee.setPrimaryContact(primaryContact);
-	// employee.setSecondaryContact(secondaryContact);
-	// employee.setSkypeId(skypeId);
-	// employee.setEnabled(enabled);
-	//
-	// if(!"".equals(password) && password.equals(confirmPassword))
-	// employee.setPassword(password);
-	// else
-	// employee.setPassword(oldPassword);
-	//
-	// EmployeeService employeeService = EmployeeServiceImpl.getInstance();
-	// employeeService.addSkills(skills, employeeCode);
-	// employeeService.updateEmployee(employee);
-	//
-	// return new ModelAndView("employee/dashboard", "email", email);
-	// }
-	//
-	// @RequestMapping(value = "/searchEmployee", method = RequestMethod.GET)
-	// public ModelAndView searchEmployee(){
-	//
-	// return new ModelAndView("employee/searchEmployee");
-	// }
-	//
-	// @RequestMapping(value = "/searchEmployee", method = RequestMethod.POST)
-	// public ModelAndView searchEmployee(@RequestParam("criteria") String
-	// criteria, @RequestParam("keyword") String keyword){
-	//
-	// EmployeeService employeeService = EmployeeServiceImpl.getInstance();
-	// List<Employee> searchResult = employeeService.searchEmployee(criteria,
-	// keyword);
-	//
-	// return new ModelAndView("employee/searchResults", "result",
-	// searchResult);
-	// }
-	//
-	// @RequestMapping(value = "/viewEmployee", method = RequestMethod.GET)
-	// public ModelAndView viewEmployee(@RequestParam String employeeCode){
-	//
-	// EmployeeService employeeService = EmployeeServiceImpl.getInstance();
-	//
-	// return new ModelAndView("viewEmployee", "employee",
-	// employeeService.getEmployeeByCode(employeeCode));
-	// }
-
+	
 }
