ad9c5912f9ec3a2d81e8254f8a1d369f01ef60bf
meta-khushboo-saxena <41181545+meta-khushboo-saxena@users.noreply.github.com>
2018-09-11 00:43:46 +0530

    Update SkillsMaster.java

--- EmployeePortal/src/main/java/com/metacube/training/model/SkillsMaster.java
+++ EmployeePortal/src/main/java/com/metacube/training/model/SkillsMaster.java
@@ -1,5 +1,8 @@
 package com.metacube.training.model;
 
+/**
+* Class containing the attributes of SkillMaster entity
+*/
 public class SkillsMaster {
 	private int skillId;
 	private String skillName;
