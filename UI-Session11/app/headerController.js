1391435615ff2c5be0dea55e0932394c6f6fef52
meta-khushboo-saxena <41181545+meta-khushboo-saxena@users.noreply.github.com>
2018-10-17 01:34:47 +0530

    Delete headerController.js

--- UI-Session11/app/headerController.js
+++
@@ -1,9 +0,0 @@
-angular.module('shoppingApp').controller('headerController', function($scope, $http) {
-	$http.get('http://localhost:3000/cart').then(function(response){
-      $scope.cartList = response.data;
-      console.log(cartList.length)
-    });
-
-
-
-});
